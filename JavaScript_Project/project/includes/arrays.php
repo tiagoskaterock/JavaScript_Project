f339cf09a23957a4f87e44cde5280a3bbd59efeb
tiago <tiagolemespalhano@gmail.com>
2021-04-26 21:05:07 -0300

    Images added

---
+++ JavaScript_Project/project/includes/arrays.php
@@ -0,0 +1,49 @@
+<?php
+
+	// cars
+	$cars = array(
+
+		array(
+			'img' => 'car1.jpg',
+			'alt' => 'car'
+		),	
+
+		array(
+			'img' => 'car2.jpg',
+			'alt' => 'car'
+		),	
+
+		array(
+			'img' => 'car3.jpg',
+			'alt' => 'car'
+		),	
+
+		array(
+			'img' => 'car4.jpg',
+			'alt' => 'car'
+		),	
+
+		array(
+			'img' => 'car5.jpg',
+			'alt' => 'car'
+		),	
+
+		array(
+			'img' => 'car6.jpg',
+			'alt' => 'car'
+		),	
+
+		array(
+			'img' => 'car7.jpg',
+			'alt' => 'car'
+		),	
+
+		array(
+			'img' => 'car8.jpg',
+			'alt' => 'car'
+		)
+
+	);
+
+?>
+
---
+++ JavaScript_Project/project/index.php
@@ -0,0 +1,35 @@
+<?php require_once('includes/arrays.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/thumbnail-gallery.css">	
+</head>
+<body>
+
+	<div class="container">
+
+		<?php
+			foreach ($cars as $car) {
+				?>
+				<div class="carimages grid">			
+						<div class="col-sm-4 image grid-item">
+							<a href="#" class="thumbnail">						
+								<img src="images/<?= $car['img'] ?>" alt="<?= $car['alt'] ?>" class="img-responsive">
+							</a>					
+						</div>
+				</div>
+				<?php
+			}
+		?>
+		
+	</div>
+
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/masonry/4.2.2/masonry.pkgd.min.js" integrity="sha512-JRlcvSZAXT8+5SQQAvklXGJuxXTouyq8oIMaYERZQasB8SBDHZaUbeASsJWpk0UUrf89DP3/aefPPrlMR1h1yQ==" crossorigin="anonymous"></script>
+	<script src="js/scripts.js"></script>
+</body>
+</html>
