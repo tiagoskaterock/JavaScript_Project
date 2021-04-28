03a72b410b872b5b921491d6e347449b269fa976
tiago <tiagolemespalhano@gmail.com>
2021-04-28 20:07:29 -0300

    Setting up the size for the  overlay

--- js/scripts.js
+++ js/scripts.js
@@ -10,11 +10,23 @@
 
 			overlay.id = 'overlay';
 
+			// setting some styles
+			overlay.style.position = 'absolute';
+			overlay.style.top = 0;
+			overlay.style.background = 'rgba(0, 0, 0, 0.7)';
+			overlay.style.cursor = 'pointer';
+
+			// setting some sizes
+			overlay.style.width = window.innerWidth + 'px';
+			overlay.style.height = window.innerWidth + 'px';
+
+			// console.log(overlay.style.background);
+
 			document.body.appendChild(overlay);
 
 		}
 
-		console.log(e.target.tagName);
+		// console.log(e.target.tagName);
 
 	});
 
